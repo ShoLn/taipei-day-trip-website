a8947dab433634dd4e7dc6263218989e57f7d9b4
Sho Ln <learningsyo@gmail.com>
2022-03-04 11:48:42 +0800

    test.py

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 #測試用老師請忽略此頁
-import mysql.connector
-db = mysql.connector.connect(
-    host="localhost",
-    port="3306",
-    user="root",
-    password="password",
-    database="website",
-    auth_plugin="mysql_native_password"
-)
-cursor = db.cursor(dictionary=True,buffered=True)
+from flask import *
 
-sql = 'SELECT * FROM `taipei_trip` WHERE `name` LIKE %s LIMIT %s,%s;'
-val = ('%'+'sfjskjfsd'+'%', 0, 3)
-cursor.execute(sql,val)
-ans = cursor.fetchone()
-print(ans)
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'yayayay'
+
+app.run(host='0.0.0.0', port=3000)
